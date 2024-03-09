4018d09d1a768478706a56af758f5927dab9a3ee
Arhan Jain <arhan23118@iiitd.ac.in>
2024-03-09 15:41:32 +0530

    Splitting the instructions for function call

--- InstructionCallingArhan.py
+++ InstructionCallingArhan.py
@@ -1,25 +1,76 @@
-lines = [['add','rd,rs1,rs2'],]  #stores all lines from the input file
+lines = [['add','rd,rs1,rs2'],['hi:','lw','rd,imm(rs1)'],['jal','rd,imm']]  #stores all lines from the input file
+register = {}
 
 def label(lines):
     labels = {}
     for a in range(len(lines)):
         if lines[a][0][-1] == ':':
             if lines[a][0][:-1] not in labels:
-                labels[lines[a][0][:-1]] = [a+1]
+                labels[lines[a][0][:-1]] = [a]
             else:
-                labels[lines[a][0][:-1]] += [a+1]
+                labels[lines[a][0][:-1]] += [a]
+            lines[a] = lines[a][1:]
     return labels
 
 
-def main(lines):
+def main(lines, register):
     labels = label(lines)
     
     for a in range(len(lines)):
         if lines[a][0][-1] != ':':
             instruction = lines[a][0]
-            reg = lines[a][1].split(',')
-            if len(reg) == 3:
-                ff(instruction, reg[0],reg[1],reg[2])
+            if lines[a][1].count(',') == 2:
+                reg = lines[a][1].split(',')
+                try:
+                    ff(instruction, reg[0], reg[1], int(reg[2]))
+                except:    
+                    if reg[2] in register:
+                        ff(instruction, reg[0], reg[1], reg[2])
+                    elif reg[2] in labels:
+                        min = labels[reg[2]][0]
+                    
+                        for b in labels[reg[2]]:
+                            if abs(b-a) < abs(min-a):
+                                min = b
+                        
+                        ff(instruction, reg[0], reg[1], 4*(a-min))
+                    
+                    else:
+                        ff(instruction, reg[0], reg[1], 'invalid')
             
-
-main(lines)
+            elif '(' in lines[a][1] and ')' in lines[a][1]:
+                reg = lines[a][1].split(',')
+                reg2 = reg[1].split('(')
+                try:
+                    ff(instruction, reg[0], reg2[1][:-1], int(reg2[0]))
+                except:
+                    ff(instruction, reg[0], reg2[1][:-1], 'InvalidImmediateValue')
+            
+            elif lines[a][1].count(',') == 1:
+                reg = lines[a][1].split(',')
+                try:
+                    ff(instruction, reg[0], int(reg[1]))
+                except:    
+                    if reg[1] in register:
+                        ff(instruction, reg[0], reg[1])
+                    elif reg[1] in labels:
+                        min = labels[reg[1]][0]
+                    
+                        for b in labels[reg[1]]:
+                            if abs(b-a) < abs(min-a):
+                                min = b
+                        
+                        ff(instruction, reg[0], 4*(a-min))
+                    
+                    else:
+                        ff(instruction, reg[0], 'invalid')
+            
+            
+                
+                
+            
+            
+            
+# add rd, rs1, rs2
+# lw rd, imm[11:0](rs1)
+# jal rd, imm[20:1]
