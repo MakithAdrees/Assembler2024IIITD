613d9e278b075b199b65bfe35ed1f492f42a804f
Arnesh Batra <66691657+arnesh2212@users.noreply.github.com>
2024-03-09 17:32:53 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -68,6 +68,8 @@ def main(lines, register):
     labels = label(lines)
     
     for a in range(len(lines)):
+        if len(lines[a]) == 0 :
+            continue
         if len(lines[a]) > 2:
             print('InvalidInstruction at line', a+1)
 
