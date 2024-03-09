34b6af152b8d2a052c8317c5415d5eb0b618af93
Arnesh Batra <66691657+arnesh2212@users.noreply.github.com>
2024-03-09 17:30:27 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -30,6 +30,14 @@ for a in range(len(lines)):
     lines[a] = lines[a].split(' ')
 
 
+def LabelToImm(labels, a, label):
+    lst = labels[label]
+    minn = lst[0]
+    for i in lst:
+        if abs(i-a) < abs(minn-a):
+            minn = i
+    return minn
+
 register = {}
 
 def label(lines):
