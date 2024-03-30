b11b00069b1208f281702caa8e91c33196338f26
Arhan Jain <147750466+MakithAdress@users.noreply.github.com>
2024-03-30 18:43:12 +0530

    Beginning of simulator (work by gp2)

---
+++ simulator_instructions_gp2.py
@@ -0,0 +1,47 @@
+def DecToBin(decimal):
+    binary = bin(decimal)[2:]
+    return binary
+def BinToDec(binary):
+    decimal = "0000"
+    return decimal
+
+def addi(code, registers):
+    reg = registers[code[-20:-15]]        #finding registers
+    imm = BinToDec(code[:12])                       #finding immediate
+    sum = imm+reg         #finding new rd
+    registers[code[-12:-7]] = sum         #rd updated
+    return registers
+    # print("addi")
+
+def lw(code, registers):
+    #arnesh will do it
+    #will do it on 3 april
+    print("lw")
+    
+def sltiu(code, registers):
+    reg = UBinToDec(code[-20:-15])        #finding registers
+    imm = code[:12]
+    if imm > reg:
+        registers[code[-12:-7]] = 1
+    return registers
+    # print("sltiu")
+    
+def jalr(code, registers):
+    reg = registers["00101"]
+    pc = registers["PC"]
+    imm = BinToDec(code[:12])
+    registers[code[-12:-7]] = pc+4
+    registers["PC"] = reg + imm
+    return registers
+    # print("jalr")
+    
+    
+def I_Type(code, registers):
+    iType = ["000","010","011","addi(code, registers)","jalr(code, registers)","lw(code, registers)","sltiu(code, registers)"]
+    s = iType.index(code[-15:-12])
+    iterate = 4
+    if code[-7:] == "0010011":
+        iterate = 3
+    eval(iType[s+iterate])
+
+I_Type('1111111110010111 000 000 1100111',{})
